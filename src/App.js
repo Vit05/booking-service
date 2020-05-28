0df93afe0309cb4a9bbc1a14bc382ff4e84ee279
Viktor Paliichuk <vpaliichuk@provectus.com>
2020-05-28 11:53:37 +0300

    Init one hour service duration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Calendar from "./components/calendar";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Calendar />
-      </div>
-    );
-  }
+
+    render() {
+        const useStyles = makeStyles((theme) => ({
+            root: {
+                display: 'flex',
+                flexDirection: 'column',
+                minHeight: '100vh',
+            },
+            main: {
+                marginTop: theme.spacing(8),
+                marginBottom: theme.spacing(2),
+            },
+            footer: {
+                padding: theme.spacing(3, 2),
+                marginTop: 'auto',
+                backgroundColor:
+                    theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+            },
+        }));
+        return (
+            <div className={useStyles.root}>
+                <Container component="main" maxWidth="md" className={useStyles.main}>
+                    <Calendar/>
+
+                </Container>
+                <footer className={useStyles.footer}>
+                    <Container maxWidth="sm">
+                        <Typography variant="body1">My sticky footer can be found here.</Typography>
+                    </Container>
+                </footer>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/services/event-booking-service.js
+++ src/services/event-booking-service.js
@@ -3,113 +3,147 @@ export default class EventBookingService {
     data = [
         {
             id: "iddd21",
-            title: "Manicure",
+            title: "Валерия Григорян",
             start: new Date(2020, 4, 25, 17, 0, 0),
             end: new Date(2020, 4, 25, 18, 0, 0),
             startVal: new Date(2020, 4, 25, 17, 0, 0).valueOf(),
             endVal: new Date(2020, 4, 25, 18, 0, 0).valueOf(),
             curDay: new Date(2020, 4, 25, 0, 0, 0).valueOf(),
-
-            desc: "some description",
+            desc: {value:"hair_cut",text: "Стрижка"}
         },
         {
             id: "iddd22",
-            title: "Manicure 2",
+            title: "Света Васильева",
             start: new Date(2626, 4, 26, 10, 0, 0),
             end: new Date(2626, 4, 26, 11, 0, 0),
             startVal: new Date(2626, 4, 26, 10, 0, 0).valueOf(),
             endVal: new Date(2626, 4, 26, 11, 0, 0).valueOf(),
             curDay: new Date(2626, 4, 26, 0, 0, 0).valueOf(),
-
-            desc: "some description",
+            desc: {value:"pedicure",text: "Педикюр"}
         },
         {
             id: "iddd23",
-            title: "Manicure 1",
+            title: "Станислав",
             start: new Date(2020, 4, 28, 12, 0, 0),
             end: new Date(2020, 4, 28, 13, 0, 0),
             startVal: new Date(2020, 4, 28, 12, 0, 0).valueOf(),
             endVal: new Date(2020, 4, 28, 13, 0, 0).valueOf(),
             curDay: new Date(2020, 4, 28, 0, 0, 0).valueOf(),
-
-            desc: "some description 3",
+            desc: {value:"hair_cut",text: "Стрижка"}
         }, {
             id: "iddd25",
-            title: "Manicure 2",
+            title: "Вика Серова",
             start: new Date(2020, 4, 28, 14, 0, 0),
             end: new Date(2020, 4, 28, 15, 0, 0),
             startVal: new Date(2020, 4, 28, 14, 0, 0).valueOf(),
             endVal: new Date(2020, 4, 28, 15, 0, 0).valueOf(),
             curDay: new Date(2020, 4, 28, 0, 0, 0).valueOf(),
-
-            desc: "some description 3",
+            desc: {value:"make_up",text: "Make UP"}
         }, {
             id: "iddd26",
-            title: "Manicure 3",
+            title: "Андрей Данчук",
             start: new Date(2020, 4, 29, 10, 0, 0),
             end: new Date(2020, 4, 29, 11, 0, 0),
             startVal: new Date(2020, 4, 29, 10, 0, 0).valueOf(),
             endVal: new Date(2020, 4, 29, 11, 0, 0).valueOf(),
             curDay: new Date(2020, 4, 29, 0, 0, 0).valueOf(),
-
-            desc: "some description 3",
+            desc: {value:"barber",text: "Борода"}
         }, {
             id: "iddd27",
-            title: "Manicure 4",
+            title: "Марина Гришына",
             start: new Date(2020, 4, 29, 17, 0, 0),
             end: new Date(2020, 4, 29, 18, 0, 0),
             startVal: new Date(2020, 4, 29, 17, 0, 0).valueOf(),
             endVal: new Date(2020, 4, 29, 18, 0, 0).valueOf(),
             curDay: new Date(2020, 4, 29, 0, 0, 0).valueOf(),
-
-            desc: "some description 3",
+            desc: {value:"hair_color",text: "Окрашивание волос"}
         },
         {
             id: "iddd28",
-            title: "Manicure 45",
+            title: "Дарья Донцова",
             start: new Date(2020, 4, 22, 15, 0, 0),
             end: new Date(2020, 4, 22, 16, 0, 0),
             startVal: new Date(2020, 4, 22, 15, 0, 0).valueOf(),
             endVal: new Date(2020, 4, 22, 16, 0, 0).valueOf(),
             curDay: new Date(2020, 4, 22, 0, 0, 0).valueOf(),
-
-            desc: "some description 3",
+            desc: {value:"bla_bla_bla",text: "Просто поболтать"}
         },
         {
             id: "iddd29",
-            title: "barber",
+            title: "Сергей Власюк",
             start: new Date(2020, 4, 27, 15, 0, 0),
             end: new Date(2020, 4, 27, 16, 0, 0),
             startVal: new Date(2020, 4, 27, 15, 0, 0).valueOf(),
             endVal: new Date(2020, 4, 27, 16, 0, 0).valueOf(),
             curDay: new Date(2020, 4, 27, 0, 0, 0).valueOf(),
 
-            desc: "some description barber",
+            desc: {value:"hair_cut",text: "Стрижка"}
         },
         {
             id: "iddd30",
-            title: "Manicure Pedicure",
+            title: "Ольга Бузова",
             start: new Date(2020, 4, 27, 11, 0, 0),
             end: new Date(2020, 4, 27, 12, 0, 0),
             startVal: new Date(2020, 4, 27, 11, 0, 0).valueOf(),
             endVal: new Date(2020, 4, 27, 12, 0, 0).valueOf(),
             curDay: new Date(2020, 4, 27, 0, 0, 0).valueOf(),
+            desc: {value:"botox",text: "Ботокс"}
+        },
+        {
+            id: "iddd35",
+            title: "Галина Никина",
+            start: new Date(2020, 4, 30, 19, 0, 0),
+            end: new Date(2020, 4, 30, 20, 0, 0),
+            startVal: new Date(2020, 4, 30, 19, 0, 0).valueOf(),
+            endVal: new Date(2020, 4, 30, 20, 0, 0).valueOf(),
+            curDay: new Date(2020, 4, 30, 0, 0, 0).valueOf(),
+            desc: {value:"hair_color",text: "Окрашивание волос"}
+        },
+        {
+            id: "iddd36",
+            title: "Саша Валева",
+            start: new Date(2020, 4, 30, 13, 0, 0),
+            end: new Date(2020, 4, 30, 14, 0, 0),
+            startVal: new Date(2020, 4, 30, 13, 0, 0).valueOf(),
+            endVal: new Date(2020, 4, 30, 14, 0, 0).valueOf(),
+            curDay: new Date(2020, 4, 30, 0, 0, 0).valueOf(),
+            desc: {value:"pedicure",text: "Педикюр"}
+        },
+        {
+            id: "iddd37",
+            title: "Вадим Велиев",
+            start: new Date(2020, 4, 31, 12, 0, 0),
+            end: new Date(2020, 4, 31, 13, 0, 0),
+            startVal: new Date(2020, 4, 31, 12, 0, 0).valueOf(),
+            endVal: new Date(2020, 4, 31, 13, 0, 0).valueOf(),
+            curDay: new Date(2020, 4, 31, 0, 0, 0).valueOf(),
+            desc: {value:"hair_cut",text: "Стрижка"}
+        },
 
-            desc: "some description barber",
+        {
+            id: "iddd40",
+            title: "Алиса Городецкая",
+            start: new Date(2020, 5, 1, 10, 0, 0),
+            end: new Date(2020, 5, 1, 11, 0, 0),
+            startVal: new Date(2020, 5, 1, 10, 0, 0).valueOf(),
+            endVal: new Date(2020, 5, 1, 11, 0, 0).valueOf(),
+            curDay: new Date(2020, 5, 1, 0, 0, 0).valueOf(),
+            desc: {value:"manicure",text: "Маникюр"}
         },
 
     ]
 
     getEvents() {
         return new Promise((resolve, reject) => {
-            setTimeout(() => {
+            /*setTimeout(() => {
 
                 if (Math.random() > 0.75) {
                     reject(new Error('Some error !!!'))
                 } else {
                     resolve(this.data)
                 }
-            }, 900)
+            }, 900)*/
+            resolve(this.data)
         })
     }
 }
